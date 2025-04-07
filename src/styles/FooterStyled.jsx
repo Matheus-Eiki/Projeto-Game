5efdfee841c4bf6bcf13b59a478152922d762087
Matheus <m.eiki.ikeda@gmail.com>
2025-04-07 11:17:41 -0300

    Criando estilização do header e do footer

---
+++ src/styles/FooterStyled.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const FooterContainer =styled.div`
+
+    background:#Ffa500;
+    color:#000000;
+    padding: 20px;
+    margin-top: auto;
+
+    display:flex;
+    justify-content:space-around;
+    max-width: 1200px;
+    margin:auto;
+    h3{
+        margin-bottom: 10px;
+    
+    }
+    ul{
+        ist-style>none;
+        padding: 0px;
+    }
+
+    li{
+        margin-bottom:5px;
+    }
+
+    a{
+        color:#000000;
+        text-decoration:none;
+    }
+`
+
+
