4468d1540360f6e9e58a1ef6da13b930fbf1c727
Matheus <m.eiki.ikeda@gmail.com>
2025-04-11 16:28:01 -0300

    Complementando o site da aula

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,45 @@ import Home from './routes/Home'
 import Filtro from './routes/Filtro'
 import Jogador from './routes/Jogador'
 import Aside from './routes/Aside'
+import Dados from './Data/Dados.json'
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle =createGlobalStyle `
+  *{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+  }
+
+  body{
+    background:#f4f4f4;
+  }
+  #container{
+    display:flex;
+    flex-direction:column;
+    min-height: 100vh;
+  }
+
+  #content-wraper{
+    display:grid;
+    grid-template-column: 1fr 200px;
+    gap:20px;
+    padding:20px;
+    flex:1;
+  }
+
+  @media (max-width:768px){
+    #content-wrap{
+      grid-template-column: 1fr;
+    }
+    aside{
+      display:block;
+      width:100%;
+    }
+  }
+
+`
+
 function App() {
 
 
